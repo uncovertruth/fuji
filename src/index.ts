f293cacda765b3ca4240a4bf1b0b95c785ba9ff3
Keisuke Kan <9renpoto@gmail.com>
2018-02-02 18:09:19 +0900

    refactor(src) remove any type

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import '../node_modules/basscss/css/basscss.css'
 import './scss/screen.scss'
 
 const ACTION = window.ontouchstart === undefined ? 'click' : 'touchend'
-const toggle: any = document.getElementById('toggle')
-const overlay: any = document.getElementById('overlay')
+const toggle = document.getElementById('toggle') as HTMLElement
+const overlay = document.getElementById('overlay') as HTMLElement
 toggle.addEventListener(ACTION, () => {
   const ACTIVE = 'active'
   const OPEN = 'open'
