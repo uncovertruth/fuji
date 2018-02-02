d4f40ca011fc29da0966c559d23f776f0ea891f1
Keisuke Kan <9renpoto@gmail.com>
2018-02-02 17:53:05 +0900

    fix(src) open SP toggle menu

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 import '../node_modules/basscss/css/basscss.css'
 import './scss/screen.scss'
+
+const ACTION = window.ontouchstart === undefined ? 'click' : 'touchend'
+const toggle: any = document.getElementById('toggle')
+const overlay: any = document.getElementById('overlay')
+toggle.addEventListener(ACTION, () => {
+  const ACTIVE = 'active'
+  const OPEN = 'open'
+  if (toggle.classList.contains(ACTIVE)) {
+    toggle.classList.remove(ACTIVE)
+    overlay.classList.remove(OPEN)
+    return
+  }
+  toggle.classList.add(ACTIVE)
+  overlay.classList.add(OPEN)
+})
