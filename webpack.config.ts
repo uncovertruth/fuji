fc601245f56997368231f5d9fac1fe9fdef81c2c
Keisuke Kan <9renpoto@gmail.com>
2018-02-05 10:23:33 +0900

    fix(webpack.config) Added minimize options

--- webpack.config.ts
+++ webpack.config.ts
@@ -27,7 +27,8 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           use: [
             {
-              loader: 'css-loader'
+              loader: 'css-loader',
+              options: { minimize: true }
             },
             {
               loader: 'sass-loader'
@@ -40,7 +41,7 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
+          use: [{ loader: 'css-loader', options: { minimize: true } }]
         })
       }
     ]
